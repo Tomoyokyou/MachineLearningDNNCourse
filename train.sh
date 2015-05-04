045c739585151e0f2a7104f8fd3b90511023868d
Huang Teng-Hui <hui811116@outlook.com>
2015-05-04 17:18:46 +0800

    hui local

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-DATA=/home/hui/model/
+DATA=/home/hui/project/model/
 DATA_LARRY=/home/larry/Documents/data/MLDS_HW1_RELEASE_v1/mfcc/
 TYPE=fbank/
 TRAIN=${DATA}train/train_gender_norm.svm
@@ -8,13 +8,13 @@ TEST=${DATA}test/test_gender_norm.svm
 INDIM=69
 OUTDIM=48
 PHONUM=39
-RATE=0.005
+RATE=0.001
 BSIZE=256
-MAXEPOCH=500
+MAXEPOCH=100
 MOMENTUM="0 0.3 0.6 0.9"
 DECAYSET="1 0.9 0.81 0.72"
 DECAY=0.99999
-DIM=${INDIM}-128-64-${OUTDIM}
+DIM=${INDIM}-256-${OUTDIM}
 OUTMODEL=model/${DIM}.mdl
 
 mkdir -p model
@@ -23,15 +23,15 @@ mkdir -p model
 # *********TRAINING FROM SCRATCH***********
 # *****************************************
 
-./bin/train.app ${TRAIN} ${TEST} --rate ${RATE} --batchsize ${BSIZE} --maxEpoch ${MAXEPOCH} \
---momentum 0.9 --outName model/out.mdl --decay ${DECAY} --variance 1.0 --dim ${DIM}
+#./bin/train.app ${TRAIN} ${TEST} --rate ${RATE} --batchsize ${BSIZE} --maxEpoch ${MAXEPOCH} \
+#--momentum 0.9 --outName model/single_e50.mdl --decay ${DECAY} --variance 1.0 --dim ${DIM}
 
 # ******************************************
 # * THIS PART IS USED FOR LOADING DNN MODEL*
 # ****************************************** 
 
-#./bin/train.app ${TRAIN} ${TEST} ${LABEL} --rate ${RATE} --batchsize ${BSIZE} --maxEpoch ${MAXEPOCH} \
-#--momentum 0.6 --outName bug.mdl --decay ${DECAY} --dim ${DIM} --load model/dump.mdl
+./bin/train.app ${TRAIN} ${TEST} ${LABEL} --rate ${RATE} --batchsize ${BSIZE} --maxEpoch ${MAXEPOCH} \
+--momentum 0.6 --outName model/single256_load.mdl --decay ${DECAY} --dim ${DIM} --load model/single256_load.mdl
 
 
 echo "experiment done!"
